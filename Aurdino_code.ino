8eb79fc13fe1c1664ce8e7c4e90dc06c6c3f8b8f
sourabhkhemka <33145914+sourabhkhemka@users.noreply.github.com>
2018-07-30 14:57:57 +0530

    Update Aurdino_code.ino

--- Aurdino_code.ino
+++ Aurdino_code.ino
@@ -1,239 +1,165 @@
+/* CODE EDITED AND FINALIZED BY : D.SUKESH*/
+   
+
 #include<SoftwareSerial.h>
 
-int M = 2;
-int N = 3;
-int O = 4;
-int P = 5;
-int count=0;
+int LCW  = 2;
+int LCCW = 3;
+int RCW  = 4;
+int RCCW = 5;
+int encoderpower = 23;
+int countL=0;
+int countR=0;
 const int rx = 11;
 const int tx = 10;
-
+int flag=1;
 SoftwareSerial bluetooth(rx,tx);
 
-void setup() 
-{
-  Serial.begin(38400);
+
+void setup() {
+  Serial.begin(9600);
   bluetooth.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(18),isr,HIGH);
-  pinMode(M,OUTPUT);
-  pinMode(N,OUTPUT);
-  pinMode(O,OUTPUT);
-  pinMode(P,OUTPUT);
-}
+  attachInterrupt(digitalPinToInterrupt(18),isrL,HIGH);
+  attachInterrupt(digitalPinToInterrupt(19),isrR,HIGH);
 
-  int i=0,x, dist=0,j,n=1;
-  String a,d,deg;
-  float m=0;
-void loop() 
+  pinMode(LCW,OUTPUT);
+  pinMode(LCCW,OUTPUT);
+  pinMode(RCW,OUTPUT);
+  pinMode(RCCW,OUTPUT);
+  pinMode(encoderpower,OUTPUT);
+  digitalWrite(encoderpower,HIGH);
+  analogWrite(8,90);
+  analogWrite(9,96);
+
+}
+ 
+ int angle=45,motor=2,distance=50,i,prevAngle=0;
+ String angleTemp,motorTemp,distanceTemp;
+
+void loop()
 { 
-  dist = 0; i = 0; n = 1;
-  bluetooth.write(" "); 
-  while(bluetooth.available() == 0)
-  {
-    ;
-  }
-  a = bluetooth.readString();
-  
-  Serial.print("a = ");
-  Serial.println(a[0]);
-  
-  if(a[0]==48)
-  {
-    bluetooth.write("9");
 
-    while(bluetooth.available()==0)
-        {
-          ;
-        }
-        
-    d = bluetooth.readString();
-    
-    i = 0;
-    while(d[i]!='\0')
+    while(bluetooth.available())
     {
-      i++;
-    }
-    
-    j = i;
-    i = 0;
-    j -= 1;
-    n = 1;
-    
-    dist = 0;
-    while(j-->0)
-     {
-      n = n * 10;
-     }
-     
+      delay(10);  //REST FOR ARDUINO LOL
 
-    while(d[i]!='\0')
-    {
-      x = d[i++];
-      dist += (x-48) * n;
-      n /= 10;
-    }
+      //RECEIVING MOTOR NUMBER
+      motorTemp = bluetooth.readString();
+      Serial.print("Received MotorNumber is = ");
+      Serial.println(motorTemp);
+      bluetooth.write("2");  ///MOTOR NUMEBER RECEIVED
 
-    Serial.print("dist = ");
-    Serial.println(dist);
+          motor = motorTemp[0]-'0';
 
-    m = 0;
-    movecar();
-  }
 
-if(a[0]==49)
-  {
-    bluetooth.write("9");
+      //RECEVING DISTANCE
+      distanceTemp = bluetooth.readString();
+      Serial.print("Received Distance is = ");
+      Serial.println(distanceTemp);
+      bluetooth.write("3");  ///DISTANCE RECEIVED
+           for(i=0;i<4;i++)
+              {
+                if(i==0)
+                   distance =  ( distanceTemp[i] - '0' );
+                if(i==1)
+                   distance += ( distanceTemp[i] - '0' )*10;
+                if(i==2)
+                   distance += ( distanceTemp[i] - '0' )*100;
+                if(i==3)
+                   distance += ( distanceTemp[i] - '0' )*1000;
+              }
+      
+      //RECEIVING ANGLE
+      angleTemp = bluetooth.readString();
+      Serial.print("Received angle is = ");
+      Serial.println(angleTemp);
 
-    while(bluetooth.available()==0)
-        {
-          ;
-        }
-    d = bluetooth.readString();
-
-    i = 0;
-    
-    while(d[i]!='\0')
-    {
-      i++;
-    }
-
-    j = i;
-    i = 0;
-    j -= 1;
-   
-    dist = 0;
-    n=1;
-    while(j-->0)
-     {
-      n = n * 10;
-     }
-     
-
-    while(d[i]!='\0')
-    {
-      x = d[i++];
-      dist += (x-48) * n;
-      n /= 10;
-    }
-
-    Serial.print("dist = ");
-    Serial.println(dist);
-
-    m = 90;
-    movecar();
-  }
-
-if(a[0]==50)
-  {
-//    Serial.println(F("+++"));
-    bluetooth.write("8");
-
-    while(bluetooth.available()==0)
-        {
-          ;
-        }
-    d = bluetooth.readString();
-
-    i = 0;
-    while(d[i]!='\0')
-    {
-      i++;
-    }
-
-    j = i;
-    i = 0;
-    j -= 1;
-   
-    dist = 0;
-    n = 1;
-    while(j-->0)
-     {
-      n = n * 10;
-     }
-     
-
-    while(d[i]!='\0')
-    {
-      x = d[i++];
-      dist += (x-48) * n;
-      n /= 10;
-    }
-
-    Serial.print("dist = ");
-    Serial.println(dist);
-    bluetooth.write(" ");  
-    while(bluetooth.available() == 0)
-    {
-      ;
-    }
-    deg = bluetooth.readString();
-    i = 0;
-    while(deg[i]!='\0')
-    {
-      i++;
-    }
-    j = i;
-    i = 0;
-    j -= 1;
-    n = 1;
-    m = 0;
-    while(j-->0)
-     {
-      n = n * 10;
-     }
-    while(deg[i]!='\0')
-    {
-      x = deg[i++];
-      m += (x-48) * n;
-      n /= 10;
-    }    
-    Serial.print("m = ");
-    Serial.println(m);  
-    movecar();
-  }
+      bluetooth.write("1");  ///ANGLE RECEIVED ACKNOWLEDGEMENT
+            for(i=0;i<3;i++)
+              {
+                if(i==0)
+                   angle = ( angleTemp[i] - '0' );
+                if(i==1)
+                   angle += ( angleTemp[i] - '0' )*10;
+                if(i==2)
+                   angle += ( angleTemp[i] - '0' )*100;
+              }
+      
+    }*/
+    control();
+//    delay(1000);
 }
 
 
-
-
-
-
-
-//********************///
-void movecar()
+void control()
 {
-  while(count<(0.8*m))
-   {
-    Serial.println("tttttttttttttttttttt");
-    digitalWrite(M,HIGH);
-    digitalWrite(N,LOW);
-    digitalWrite(O,LOW);
-    digitalWrite(P,LOW);
-   }
-  count=0;
-
-  if(m!=0)
-  digitalWrite(M,LOW);
-  
-  while(count<(dist/10))
+  //ANGLE ROTATION AND MOTOR SELECTION
+  if(motor == 1)
   {
-    Serial.println("ddddddddddddddddddddddddddddddddddd");
-   digitalWrite(M,HIGH);
-   digitalWrite(N,LOW);
-   digitalWrite(O,HIGH);
-   digitalWrite(P,LOW);
+    while(countL<(abs(angle-prevAngle)))
+     {
+        Serial.println("LEFT MOTOR");
+        digitalWrite(LCW,HIGH);
+        digitalWrite(LCCW,LOW);
+        digitalWrite(RCW,LOW);
+        digitalWrite(RCCW,LOW);
+      //  Serial.print(countL);
+     }
+     countL=0;
+
+     if(angle!=0)
+      digitalWrite(LCW,LOW);
+      delay(500);
   }
-  count=0;
-  
-  digitalWrite(M,LOW);
-  digitalWrite(O,LOW);
+  else if(motor == 2)
+  {
+    while(countR<abs(angle-prevAngle))
+     {
+        Serial.println("RIGHT MOTOR");
+        Serial.print(countR);
+        digitalWrite(LCW,LOW);
+        digitalWrite(LCCW,LOW);
+        digitalWrite(RCW,HIGH);
+        digitalWrite(RCCW,LOW);
+     }
+     countR=0;
+     //ROTATION DONE TURN OFF MOTOR
+      digitalWrite(RCW,LOW);
+      delay(500);
+      
+  }
+  if(flag==1)
+  {
+        countL=0;
+  //DISTANCE CALCULATION AND TRAVERSING
+    while(countL<=distance)
+     {
+    //  Serial.print("LEFT");
+     // Serial.println(countL);
+       //     Serial.print("RIGHT");
+     // Serial.println(countL);
+      digitalWrite(LCW,HIGH);
+      digitalWrite(LCCW,LOW);
+      digitalWrite(RCW,HIGH);
+      digitalWrite(RCCW,LOW);
+     }
+     countL=0;
+     countR=0;
+      //DISTANCE DONE TURN OFF BOTH MOTORS
+     digitalWrite(LCW,LOW);
+     digitalWrite(LCCW,LOW);
+     digitalWrite(RCW,LOW);
+     digitalWrite(RCCW,LOW);
+     flag=2;
+     prevAngle = angle;
+  }
 }
-//********************************//
 
-
-
-
-void isr()
+void isrL()
 {
-  count++;
+  countL++;
+}
+void isrR()
+{
+  countR++;
 }
