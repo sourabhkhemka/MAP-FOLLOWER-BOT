ac46614cd304362e48239ea9385ed1a0d0d84e45
sourabhkhemka <33145914+sourabhkhemka@users.noreply.github.com>
2018-08-12 22:39:42 +0530

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -1,31 +1,42 @@
 import serial
-import time
-import math
+#import time
+#import math
 
 port = "COM3"
 
-bluetooth = serial.Serial(port,9600);
+#bluetooth = serial.Serial(port,9600);
 print('CONNECTED');
 
 count = 0
 
 while(count<len(inst)):
-   bluetooth.write(str(inst[count][0]))
-   print(inst[count][0])
-   x = bluetooth.read()
-   if(x == '9'):
-       bluetooth.write(str(int(inst[count][1])))
-       print(int(inst[count][1]))
-   else:
-       bluetooth.write(str(int(inst[count][1])))
-       print(int(inst[count][1]))
-       bluetooth.read()
-       deg = int(90 - math.degrees(math.
-                                   atan(inst[count][2])))
-       bluetooth.write(str(deg))
-       print(int(deg))
-       
-       
-
+     
+    #SENDING MOTOR NUMBER
+   #print('###')
+   #bluetooth.read()
+   print('+++')
+   bluetooth.write(str(int(inst[count][0])))
+   #print(int(str(inst[count][0])))
+   #x = bluetooth.read()
+   #print x
+   if(bluetooth.read()=='2'):
+       print('ACKNOWLEDGED :)')
+   
+    #SENDING DISTANCE IN PIXELS
+    
+   bluetooth.write(str((int(inst[count][1]))/10))  
+   if(bluetooth.read()=='3'):
+       print('ACKNOWLEDGED :)')
+   
+    #SENDING ANGLE IN DEGREES [0,360)
+   
+   bluetooth.write(str(int(inst[count][2])))
+   if(bluetooth.read()=='1'):
+       print('ACKNOWLEDGED :)')
+    
+    #COUNTER INCREMENTATION
    count += 1
    bluetooth.read()
+
+
+
